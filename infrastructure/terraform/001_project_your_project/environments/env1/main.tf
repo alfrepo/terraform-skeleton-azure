46fb799a5a23206b041b444f57791a2c33a4f3cd
Alex <3707177+skipidar@users.noreply.github.com>
2024-03-17 18:27:27 +0100

    ignoring .terraform prefixing project

--- infrastructure/terraform/001_project_your_project/environments/env1/main.tf
+++ infrastructure/terraform/001_project_your_project/environments/env1/main.tf
@@ -4,9 +4,19 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "=> 3.96.0"
     }
   }
+
+  # use a remote state store
+
+  # backend "azurerm" {
+  #     resource_group_name  = "alfdevtfstateblob"
+  #     storage_account_name = "alfdevtfstateblob"
+  #     container_name       = "alfdevtfstateblob"
+  #     key                  = "project_az_function_terraform.tfstate"
+  # }
+
 }
 
 # Configure the Microsoft Azure Provider
